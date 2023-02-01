a4f1a8ef34dca613753a97127cc0990827d8fcdb
grammb <108758537+grammb@users.noreply.github.com>
2023-02-01 14:04:41 -0800

    point and click move controls in!

--- Assets/Scripts/dragNdrop2D.cs
+++ Assets/Scripts/dragNdrop2D.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class dragNdrop2D : MonoBehaviour
 {
-    public Rigidbody2D selectedObject;
+    public GameObject selectedObject;
     Vector3 offset;
-    Vector3 mousePosition;
     void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
             if (targetObject)
             {
-                selectedObject = targetObject.transform.gameObject.GetComponent<Rigidbody2D>();
+                selectedObject = targetObject.transform.gameObject;
                 offset = selectedObject.transform.position - mousePosition;
             }
+        }
+        if (selectedObject)
+        {
+            selectedObject.transform.position = mousePosition + offset;
         }
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
             selectedObject = null;
         }
     }
-    void FixedUpdate()
-    {
-        if (selectedObject)
-        {
-            selectedObject.MovePosition(mousePosition + offset);
-        }
-    }
+
 }
