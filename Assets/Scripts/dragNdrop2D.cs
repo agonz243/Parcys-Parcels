816fa715431c4c7919268442eb13df3c3d6b80f2
grammb <108758537+grammb@users.noreply.github.com>
2023-02-01 16:48:54 -0800

    grid snapping added, tweaking rotate

--- Assets/Scripts/dragNdrop2D.cs
+++ Assets/Scripts/dragNdrop2D.cs
@@ -7,8 +7,10 @@ public class dragNdrop2D : MonoBehaviour
     public GameObject selectedObject;
     Vector3 offset;
     Vector3 angle;
+    //Vector3 gridSnap;
     void Update()
     {
+        var rotate = 90;
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
@@ -19,23 +21,33 @@ public class dragNdrop2D : MonoBehaviour
                 offset = selectedObject.transform.position - mousePosition;
             }
         }
-        if (selectedObject)
+        if (selectedObject) //if the mouse overlaps woth a draggable object (a collider) we move it 
         {
-            selectedObject.transform.position = mousePosition + offset;
-            var rotate = 90;
-            if (Input.GetMouseButtonDown(1))
+            selectedObject.transform.position = mousePosition + offset; //doing the moving
+            if (Input.GetMouseButtonDown(1)) //right click controls for rotation
             {
-                angle = Vector3.forward * rotate;
-                selectedObject.transform.eulerAngles = angle;
-                rotate = rotate * 2;
+              
+                angle = Vector3.forward * rotate; //z axis rotation with (0, 0, 90) as vector, for 90 degree rotation
+                selectedObject.transform.eulerAngles = angle; //doing the actual rotating
+
+                
             }
             if (Input.GetMouseButtonUp(1))
             {
+
                 Debug.Log(rotate);
             }
+
         }
-        if (Input.GetMouseButtonUp(0) && selectedObject)
+        if (Input.GetMouseButtonUp(0) && selectedObject) //LMB raised while over movable object, aka dropping it
         {
+            var currentPos = selectedObject.transform.position; //fetch the current objects position
+            //gridSnap = (Mathf.Round(currentPos.x),
+            //                             Mathf.Round(currentPos.y),
+            //                             Mathf.Round(currentPos.z));
+            selectedObject.transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                         Mathf.Round(currentPos.y),
+                                         Mathf.Round(currentPos.z));
             selectedObject = null;
         }
     }
