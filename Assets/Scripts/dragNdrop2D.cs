421cccced33a5acbc9f41edef6962c1e62f0f4e9
grammb <108758537+grammb@users.noreply.github.com>
2023-02-01 17:18:49 -0800

    rotation working, grid snapping working

--- Assets/Scripts/dragNdrop2D.cs
+++ Assets/Scripts/dragNdrop2D.cs
@@ -6,11 +6,17 @@ public class dragNdrop2D : MonoBehaviour
 {
     public GameObject selectedObject;
     Vector3 offset;
-    Vector3 angle;
-    //Vector3 gridSnap;
+
     void Update()
     {
-        var rotate = 90;
+        //rotating function======================================
+        void RotateByDegrees(GameObject wespin)
+        {
+            Vector3 rotationToAdd = new Vector3(0, 0, 45);
+            wespin.transform.Rotate(rotationToAdd);
+        }
+        //========================================================
+
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
@@ -26,25 +32,17 @@ public class dragNdrop2D : MonoBehaviour
             selectedObject.transform.position = mousePosition + offset; //doing the moving
             if (Input.GetMouseButtonDown(1)) //right click controls for rotation
             {
-              
-                angle = Vector3.forward * rotate; //z axis rotation with (0, 0, 90) as vector, for 90 degree rotation
-                selectedObject.transform.eulerAngles = angle; //doing the actual rotating
 
-                
-            }
-            if (Input.GetMouseButtonUp(1))
-            {
-
-                Debug.Log(rotate);
+                RotateByDegrees(selectedObject);
+                 //doing the actual rotating
             }
 
         }
         if (Input.GetMouseButtonUp(0) && selectedObject) //LMB raised while over movable object, aka dropping it
         {
             var currentPos = selectedObject.transform.position; //fetch the current objects position
-            //gridSnap = (Mathf.Round(currentPos.x),
-            //                             Mathf.Round(currentPos.y),
-            //                             Mathf.Round(currentPos.z));
+
+            //doing grid snapping, rounding to nearest whole, need to scale by grid within bounds of our "bag' rectangle
             selectedObject.transform.position = new Vector3(Mathf.Round(currentPos.x),
                                          Mathf.Round(currentPos.y),
                                          Mathf.Round(currentPos.z));
