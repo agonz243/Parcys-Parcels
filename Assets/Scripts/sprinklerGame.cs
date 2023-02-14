9002fa62c53011868283ca598cd52090a21299db
Jooostan <67809174+Jooostan@users.noreply.github.com>
2023-02-14 15:45:42 -0800

    Started refactoring process of Path script into sprinklerGame

---
+++ Assets/Scripts/sprinklerGame.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprinklerGame : MonoBehaviour
+{
+    // Array to hold traversal points
+    [SerializeField] Transform[] Points;
+    
+    // General game / scene timers
+    private float gameTimer = 45;
+    private float hideTimer = 1;
+    private float hitTimer = 2.5F;
+
+    // Flags to start / stop timers
+    private bool hideTimerRun;
+    private bool hitTimerRun;
+
+    // Text objects to display: game timer, lives, umbrella uses
+    [SerializeField] private Text gameTimerText;
+    [SerializeField] private Text livesText;
+    [SerializeField] private Text umbrellaUseText;
+
+    public class Player{
+        private float moveSpeed;
+        private float umbrellaUse;
+        private int pointIndex;
+        private bool keyAlt;
+        private bool backKeyAlt;
+        private bool hide;
+        private bool hit;
+        private int lives;
+
+        void hit()
+        {
+            hit = true;
+            lives --;
+        }
+
+        void movement()
+        {
+            if(Input.GetKeyDown(KeyCode.A)){
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
