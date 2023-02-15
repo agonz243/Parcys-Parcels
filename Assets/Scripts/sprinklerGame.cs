b3681149e701ace1c798d1be9416c15bade9bd4e
Jooostan <67809174+Jooostan@users.noreply.github.com>
2023-02-14 16:06:14 -0800

    Temp commented out new script (mb y'all)

--- Assets/Scripts/sprinklerGame.cs
+++ Assets/Scripts/sprinklerGame.cs
@@ -1,59 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class sprinklerGame : MonoBehaviour
-{
-    // Array to hold traversal points
-    [SerializeField] Transform[] Points;
+// public class sprinklerGame : MonoBehaviour
+// {
+//     // Array to hold traversal points
+//     [SerializeField] Transform[] Points;
     
-    // General game / scene timers
-    private float gameTimer = 45;
-    private float hideTimer = 1;
-    private float hitTimer = 2.5F;
+//     // General game / scene timers
+//     private float gameTimer = 45;
+//     private float hideTimer = 1;
+//     private float hitTimer = 2.5F;
 
-    // Flags to start / stop timers
-    private bool hideTimerRun;
-    private bool hitTimerRun;
+//     // Flags to start / stop timers
+//     private bool hideTimerRun;
+//     private bool hitTimerRun;
 
-    // Text objects to display: game timer, lives, umbrella uses
-    [SerializeField] private Text gameTimerText;
-    [SerializeField] private Text livesText;
-    [SerializeField] private Text umbrellaUseText;
+//     // Text objects to display: game timer, lives, umbrella uses
+//     // [SerializeField] private Text gameTimerText;
+//     // [SerializeField] private Text livesText;
+//     // [SerializeField] private Text umbrellaUseText;
 
-    public class Player{
-        private float moveSpeed;
-        private float umbrellaUse;
-        private int pointIndex;
-        private bool keyAlt;
-        private bool backKeyAlt;
-        private bool hide;
-        private bool hit;
-        private int lives;
-
-        void hit()
-        {
-            hit = true;
-            lives --;
-        }
-
-        void movement()
-        {
-            if(Input.GetKeyDown(KeyCode.A)){
-            }
-        }
-    }
+//     public class Player{
+//         private float moveSpeed;
+//         private float umbrellaUse;
+//         private int pointIndex;
+//         private bool keyAlt;
+//         private bool backKeyAlt;
+//         private bool hide;
+//         private bool hit;
+//         private int lives;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
+
+//         void playerHit()
+//         {
+//             hit = true;
+//             lives --;
+//         }
+
+//         void movement()
+//         {
+//             if(!hide && !hit){
+//                 // Moving forward
+//                 if(Input.GetKeyDown(KeyCode.A) && keyAlt == false){
+//                     transform.position = Vector2.MoveTowards(transform.position, Points[pointIndex].transform.position, moveSpeed);
+//                     keyAlt = true;
+//                 } else if(Input.GetKeyDown(keyCode.D) && keyAlt == true){
+//                     transform.position = Vector2.MoveTowards(transform.position, Points[pointIndex].transform.position, moveSpeed);
+//                     keyAlt = false;
+//                 }
+
+//                 // Moving backwards
+//                 else if(Input.GetKeyDown(keyCode.Z) && keyAlt == false)
+//                 {
+//                     transform.position = Vector2.MoveTowards(transform.position, Points[pointIndex - 1].transform.position, moveSpeed);
+//                     backKeyAlt = true;
+//                 } else if(Input.GetKeyDown(keyCode.C) && keyAlt){
+//                     transform.position = Vector2.MoveTowards(transform.position, Points[pointIndex - 1].transform.position, moveSpeed);
+//                     backKeyAlt = false;
+//                 }
+//             }
+//         }
+
+//     }
+
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
         
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//     // Update is called once per frame
+//     void Update()
+//     {
+
+//     }
+// }
