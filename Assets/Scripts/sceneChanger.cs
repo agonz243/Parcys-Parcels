fdc91d7851a9e58f2c5ab0ed38c0e490f2587a81
juliekhou <jkhou@ucsc.edu>
2023-02-16 18:39:58 -0800

    sounds for title screen done

--- Assets/Scripts/sceneChanger.cs
+++ Assets/Scripts/sceneChanger.cs
@@ -8,9 +8,37 @@ public class sceneChanger : MonoBehaviour
 
 	Scene currentScene;
 	string sceneName;
+
+    public float delay = 0.0f;
+    public float timer;
+    public bool startTimer = false;
+
+    public AudioClip clip;
+    public AudioSource source;
     
     private void Start() {
     	currentScene = SceneManager.GetActiveScene();
+    }
+
+    void Update(){
+        Debug.Log(timer);
+        if (startTimer == true){
+            Debug.Log(timer);
+            Debug.Log("Delay: " + delay);
+            // LoadNextScene();
+            timer += Time.deltaTime;
+            if (timer > 1f){
+                LoadNextScene();
+                timer = 0;
+            }
+        }
+        
+    }
+
+    public void Wait(){
+        // source.PlayOneShot(clip);
+        startTimer = true;
+        Debug.Log("Real delay: " + delay);
     }
 
     public void LoadNextScene() {
@@ -21,8 +49,8 @@ public class sceneChanger : MonoBehaviour
         } else if (currentScene.name == "DogInstructions") {
             SceneManager.LoadScene("DogChaseDemo");
         } else if (currentScene.name == "LoseDogGame" || currentScene.name == "WinDogGame") {
-    		SceneManager.LoadScene("SprinklerInstructions");
-    	} else if (currentScene.name == "SprinklerInstructions") {
+            SceneManager.LoadScene("SprinklerInstructions");
+        } else if (currentScene.name == "SprinklerInstructions") {
             SceneManager.LoadScene("SprinklerV2");
         } else if (currentScene.name == "Win" || currentScene.name == "Lose") {
             SceneManager.LoadScene("MailbagInstructions");
@@ -33,5 +61,6 @@ public class sceneChanger : MonoBehaviour
         } else { 
             Debug.Log("Loading nothing :(");
         }
+        
     }
 }
