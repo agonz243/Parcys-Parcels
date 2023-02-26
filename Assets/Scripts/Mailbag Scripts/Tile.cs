723ad13a3928ae0409d246ecf8663cc3ab00ffd4
grammb <108758537+grammb@users.noreply.github.com>
2023-02-26 15:01:35 -0800

    fixing

--- Assets/Scripts/Mailbag Scripts/Tile.cs
+++ Assets/Scripts/Mailbag Scripts/Tile.cs
@@ -14,6 +14,8 @@ public class Tile : MonoBehaviour
 
     [SerializeField] public Collider2D bonk;
 
+    [SerializeField] public ContactPoint2D[] bonkArray = new ContactPoint2D[9]; //here can tweak size of array for number of pieces
+
     [SerializeField] public bool isSolved = false;
 
      public void Init(bool isOffset)
@@ -33,7 +35,8 @@ public class Tile : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Packages")
+        bonk = gameObject.GetComponent<Collider2D>();
+        if (other.gameObject.tag == "Packages" && bonk.GetContacts(bonkArray) == 0)//AND THERE IS NOT ANOTHER PACKAGE ON THE SQUARE
         {
             isSolved = false;
             Debug.Log("NO YOINKIES");
