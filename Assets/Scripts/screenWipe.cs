893574280ec3f23cf8e9319e6aa04440df9d1a90
Aaron Gonzales <agonz243@ucsc.edu>
2023-03-07 14:24:32 -0800

    Added scene switch when truck covers screen

--- Assets/Scripts/screenWipe.cs
+++ Assets/Scripts/screenWipe.cs
@@ -19,6 +19,7 @@ public class screenWipe : MonoBehaviour
     {
         isDone = true;
         DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(image);
     }
 
     // Update is called once per frame
@@ -41,11 +42,22 @@ public class screenWipe : MonoBehaviour
     {
         wipeProgress += Time.deltaTime * (1f / wipeSpeed);
         image.transform.position += new Vector3(0.3f, 0, 0);
-        if (wipeProgress >= 5f)
+        if (wipeProgress >= 6f)
         {
             isDone = true;
+        } else if (wipeProgress >= 3f)
+        {
+            this.GetComponent<sceneChanger>().LoadNextScene();
         }
     }
+
+/*     private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "transitioner")
+        {
+            Debug.Log("Change Scene Now!");
+            this.GetComponent<sceneChanger>().LoadNextScene();
+        }
+    } */
 
     [ContextMenu("Wipe")]
     private void Wipe() { ToggleWipe(); }
