77edcff79cc74bf32ff6e1f005bb05a429507486
Aaron Gonzales <agonz243@ucsc.edu>
2023-05-09 12:54:53 -0700

    Fixed pause canvas consistency across scenes

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
@@ -13,6 +14,27 @@ public class PauseGame : MonoBehaviour
         DontDestroyOnLoad(pauseCanvas);
     }
 
+    // On enable, subscribe to event listener that tracks scene loads
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // Function to be passed to sceneLoaded delegate
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+
+        // Get scene's main camera
+        Camera sceneCam;
+        sceneCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        // Assign main camera to pause canvas
+        Canvas pCanvas;
+        pCanvas = pauseCanvas.GetComponent<Canvas>();
+        pCanvas.worldCamera = sceneCam;
+    }
+
     void Update() 
     {
         // MUST ADD: Add setting time scale to 0 to actually pause game
@@ -21,8 +43,9 @@ public class PauseGame : MonoBehaviour
         }
     }
 
-    public void ExitGame() 
+    void OnDisable()
     {
-        Application.Quit();
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
