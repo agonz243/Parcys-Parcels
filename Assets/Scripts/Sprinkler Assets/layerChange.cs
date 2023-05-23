6c3feb0f1e07cc25b71c6502706f0b0a76745b6b
Jooostan <67809174+Jooostan@users.noreply.github.com>
2023-05-23 14:17:55 -0700

    sprinkler tweaks, added don't touch grass sign

---
+++ Assets/Scripts/Sprinkler Assets/layerChange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layerChange : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] SpriteRenderer sprite;
+    private sprinklerGame script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = player.GetComponent<sprinklerGame>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Layer: " + sprite.sortingLayerName);
+        if(script.countdownTimeLeft > 0){
+            sprite.sortingLayerName = "AboveBG";
+        }
+        else if(script.myPlayer.getPointIndex() > 10){
+            sprite.sortingLayerName = "bg-top";
+        } else{
+            sprite.sortingLayerName = "bg-bottom";
+        }
+    }
+}
