f547a285496a5c990c92b84c30482cb376f7f2e4
julie ! <jkhou@ucsc.edu>
2023-06-05 19:10:19 -0700

    added in sprinkler sounds & coded in scribble

--- Assets/Scripts/LetterInputScene.cs
+++ Assets/Scripts/LetterInputScene.cs
@@ -15,6 +15,11 @@ public class LetterInputScene : MonoBehaviour
     public PageSwitch pageTurn;
 
     public sceneChanger sceneChanger;
+
+    public AudioSource scribble;
+    public bool scribblePlayed;
+    public bool typing;
+    public float pauseDelay;
     
     private void Awake() {
         // if (letterInputScene == null) {
@@ -23,6 +28,47 @@ public class LetterInputScene : MonoBehaviour
         // } else {
         //     Destroy(gameObject);
         // }
+    }
+
+    void Start()
+    {
+        //Fetch the Input Field component from the GameObject
+        // input = GetComponent<TMP_InputField>();
+
+        scribblePlayed = false;
+        typing = false;
+    }
+
+    void Update()
+    {
+        if (typing){
+            pauseDelay -= Time.deltaTime;
+        }
+        
+        //Check if the Input Field is in focus and able to alter
+        if (input.isFocused && Input.anyKeyDown) {
+            if (!scribble.isPlaying & !scribblePlayed){
+                scribble.Play();
+                scribblePlayed = true;
+                typing = true;
+                Debug.Log("scribble");
+            } else if (!scribble.isPlaying){
+                scribble.Play();
+                typing = true;
+                Debug.Log("unpause scribble");
+                
+            }
+        } else {
+            if(pauseDelay <= 0) {
+                pauseDelay = 1;
+                typing = false;
+                scribble.Pause();
+                // Debug.Log("nothing");
+            }
+    
+        }
+
+        
     }
 
     public void saveLetter() {
